517bc0215585d6a8034bfcb2fb2a0628e3de6845
joker096 <zaartyom@yandex.ru>
2026-05-14 18:46:06 +0300

    chore

--- do-all.cjs
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-try { execSync('git add -A', { encoding: 'utf8', stdio: 'inherit' }); } catch(e) {}
-try { execSync('git commit -m "fix: add null checks for e.date.split() in Profile calendar filters, exclude 404.html from PWA precache"', { encoding: 'utf8', stdio: 'inherit' }); } catch(e) {}
-try { execSync('git push', { encoding: 'utf8', stdio: 'inherit' }); } catch(e) {}
